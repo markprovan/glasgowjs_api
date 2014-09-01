df85336298938eb14cb26bbc6efe8336786642a8
Mark Provan <markgprovan@gmail.com>
2014-09-01 20:46:49 +0100

    Added support for cors

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/codegangsta/martini-contrib/cors"
 	"github.com/go-martini/martini"
 	"github.com/jmoiron/sqlx"
 	"github.com/martini-contrib/render"
@@ -13,6 +14,13 @@ func main() {
 	m.Map(SetupDB())
 	m.Get("/posts", PostsIndex)
 	m.Get("/posts/:id", PostsShow)
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{"GET", "POST", "PUT"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	m.Run()
 }
 
